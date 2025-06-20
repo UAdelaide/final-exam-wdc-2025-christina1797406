51c9f101896676ded2d98856efba34e7b04146ac
christina1797406 <a1797406@adelaide.edu.au>
2025-06-20 05:38:57 +0000

    [automatic commit] 2025-06-20 05:38:57 UTC 203.214.152.50

--- part1/app.js
+++ part1/app.js
@@ -82,7 +82,7 @@ let db;
 // Route 1 ('/api/dogs')
 app.get('/api/dogs', async (req, res) => {
   try {
-    const [rows] = await db.execute('SELECT name, size, ');
+    const [rows] = await db.execute('SELECT Dogs.name, Dogs.name, ');
 
     res.json(rows);
   } catch (err) {
