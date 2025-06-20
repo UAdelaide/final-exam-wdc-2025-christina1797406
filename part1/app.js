023e4f6d996dc27845a50cbd06edb9676bb7e750
christina1797406 <a1797406@adelaide.edu.au>
2025-06-20 05:52:46 +0000

    [automatic commit] 2025-06-20 05:52:46 UTC 203.214.152.50

--- part1/app.js
+++ part1/app.js
@@ -76,7 +76,7 @@ let db;
     VALUES ('Rocky', 'medium', (SELECT user_id FROM Users WHERE username = 'bobwalker'));
     `);
 
-    
+    // Insert WalkRequests
     await db.execute(`
     INSERT IGNORE INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
         VALUES
