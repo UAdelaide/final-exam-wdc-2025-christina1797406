669f809d6491a496645689038f9cc264a86ee19e
christina1797406 <a1797406@adelaide.edu.au>
2025-06-20 10:30:55 +0000

    [automatic commit] 2025-06-20 10:30:55 UTC 203.214.152.50

--- part1/app.js
+++ part1/app.js
@@ -204,8 +204,11 @@ app.get('/api/walkers/summary', async (req, res) => {
       SELECT COUNT(*) AS total_ratings FROM WalkRequests
       SELECT AVE(total_ratings) AS average_rating
 
-      SELECT Users.username AS walker_username
-      
+      SELECT
+        Users.username AS walker_username
+        COUNT()
+        ROUND()
+        
 
 
 
