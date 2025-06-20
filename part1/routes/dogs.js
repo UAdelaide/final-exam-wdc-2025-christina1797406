fa33597f0f3d45b0ba717f02d574a64ce8e41549
christina1797406 <a1797406@adelaide.edu.au>
2025-06-20 05:16:21 +0000

    [automatic commit] 2025-06-20 05:16:21 UTC 203.214.152.50

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -12,7 +12,7 @@ var db = require('../database');
     });
 
     // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS testdb');
+    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
     await connection.end();
 
     // Now connect to the created database
