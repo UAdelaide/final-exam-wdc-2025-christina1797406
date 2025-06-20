c5cb51403a201c1424e1ec7aaa15133e11e44626
christina1797406 <a1797406@adelaide.edu.au>
2025-06-20 05:13:15 +0000

    [automatic commit] 2025-06-20 05:13:15 UTC 203.214.152.50

--- part1/database.js
+++ part1/database.js
@@ -3,5 +3,6 @@ var mysql = require('mysql2/promise');
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: ''
+    password: '',
+    database: 'DogWalkService'
 })
