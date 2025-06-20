078fc027b059d51b78aaa4c0adb1bcb6a74905ea
christina1797406 <a1797406@adelaide.edu.au>
2025-06-20 11:15:13 +0000

    [automatic commit] 2025-06-20 11:15:13 UTC 203.214.152.50

--- part1/app.js
+++ part1/app.js
@@ -144,8 +144,8 @@ let db;
     if (row_request[0].count === 0) {
         await db.execute(`
         INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
-        ((SELECT walker_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
-        ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+        ((SELECT walker_id FROM WalkRatings WHERE walker_id = 'bobwalker'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+        ((SELECT walker_id FROM WalkRatings WHERE walker_id = 'bobwalker'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
         ((SELECT dog_id FROM Dogs WHERE name = 'Charlie'), '2025-06-10 10:00:00', 20, 'Parklands', 'open'),
         ((SELECT dog_id FROM Dogs WHERE name = 'Happy'), '2025-06-10 10:30:00', 30, 'Ascot Park', 'cancelled'),
         ((SELECT dog_id FROM Dogs WHERE name = 'Rocky'), '2025-06-10 10:45:00', 40, 'Ascot Park', 'cancelled')
