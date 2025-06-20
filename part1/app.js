09f5a6e32d95464b7b05c79431c925ea5449ec2f
christina1797406 <a1797406@adelaide.edu.au>
2025-06-20 10:39:35 +0000

    [automatic commit] 2025-06-20 10:39:35 UTC 203.214.152.50

--- part1/app.js
+++ part1/app.js
@@ -164,7 +164,7 @@ app.get('/api/dogs', async (req, res) => {
     SELECT
         Dogs.name AS dog_name,
         Dogs.size AS size,
-        
+        Users.username AS owner_username
     Dogs.name, Dogs.size, Users.username FROM Dogs
     INNER JOIN Users
     ON Dogs.owner_id = Users.user_id;
