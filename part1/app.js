d293f73d074307044e191c6801b21db9a0e67b3d
christina1797406 <a1797406@adelaide.edu.au>
2025-06-20 08:15:03 +0000

    [automatic commit] 2025-06-20 08:15:03 UTC 203.214.152.50

--- part1/app.js
+++ part1/app.js
@@ -180,8 +180,8 @@ app.get('/api/walkrequests/open', async (req, res) => {
       WalkRequests.requested_time, WalkRequests.duration_minutes,
       WalkRequests.location, Users.username FROM (( WalkRequests
       INNER JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id )
-      INNER JOIN Users ON 
-      ON
+      INNER JOIN Users ON Dogs.owner_id = Users.user_id )
+      
 
       `);
 
