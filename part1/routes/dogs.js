5fb89d2dcb772d60447b65c029f8cbd101146161
christina1797406 <a1797406@adelaide.edu.au>
2025-06-20 05:15:20 +0000

    [automatic commit] 2025-06-20 05:15:20 UTC 203.214.152.50

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var db = require('../database');
 
-
 (async () => {
   try {
     // Connect to MySQL without specifying a database
