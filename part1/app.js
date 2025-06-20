0aba24ec6f045682d1c20115b9d7636359cc9cc1
christina1797406 <a1797406@adelaide.edu.au>
2025-06-20 06:33:16 +0000

    [automatic commit] 2025-06-20 06:33:16 UTC 203.214.152.50

--- part1/app.js
+++ part1/app.js
@@ -4,9 +4,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mysql = require('mysql2/promise');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 const PORT = 8080;
 
@@ -15,8 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // Database db
 let db;
