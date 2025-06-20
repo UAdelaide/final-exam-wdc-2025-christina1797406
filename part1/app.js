e140d3c27482c5b5c79ae7245d82e4ae844dfbaf
christina1797406 <a1797406@adelaide.edu.au>
2025-06-20 06:25:17 +0000

    [automatic commit] 2025-06-20 06:25:17 UTC 203.214.152.50

--- part1/app.js
+++ part1/app.js
@@ -78,7 +78,7 @@ let db;
 
     // WalkApplication
     await db.execute(`
-    CREATE TABLE WalkApplications
+    CREATE TABLE IF NOT EXISTS WalkApplications
         application_id INT AUTO_INCREMENT PRIMARY KEY,
         request_id INT NOT NULL,
         walker_id INT NOT NULL,
@@ -87,10 +87,23 @@ let db;
         FOREIGN KEY (request_id) REFERENCES WalkRequests(request_id),
         FOREIGN KEY (walker_id) REFERENCES Users(user_id),
         CONSTRAINT unique_application UNIQUE (request_id, walker_id)
-    );`)
+    );`);
 
     // WalkRatings
-    await db.execute(``)
+    await db.execute(`
+    CREATE TABLE WalkRatings (
+        rating_id INT AUTO_INCREMENT PRIMARY KEY,
+        request_id INT NOT NULL,
+        walker_id INT NOT NULL,Add commentMore actions
+        owner_id INT NOT NULL,
+        rating INT CHECK (rating BETWEEN 1 AND 5),
+        comments TEXT,
+        rated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (request_id) REFERENCES WalkRequests(request_id),
+        FOREIGN KEY (walker_id) REFERENCES Users(user_id),
+        FOREIGN KEY (owner_id) REFERENCES Users(user_id),
+        CONSTRAINT unique_rating_per_walk UNIQUE (request_id)
+    );`);
 
 // Insert tables if database is empty
     // Insert Users
