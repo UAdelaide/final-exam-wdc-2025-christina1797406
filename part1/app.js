608dd44f1730c52ad093e9d3df8e4b4aaa420cab
christina1797406 <a1797406@adelaide.edu.au>
2025-06-20 05:29:08 +0000

    [automatic commit] 2025-06-20 05:29:08 UTC 203.214.152.50

--- part1/app.js
+++ part1/app.js
@@ -79,7 +79,7 @@ let db;
   }
 });
 
-// Route to return books as JSON
+// Route path ('/api/dogs')
 app.get('/', async (req, res) => {
   try {
     const [books] = await db.execute('SELECT * FROM books');
