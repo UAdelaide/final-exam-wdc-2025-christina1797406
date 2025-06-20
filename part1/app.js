f48f906c4875ffd27259c343aebcdffb16880a83
christina1797406 <a1797406@adelaide.edu.au>
2025-06-20 06:23:35 +0000

    [automatic commit] 2025-06-20 06:23:35 UTC 203.214.152.50

--- part1/app.js
+++ part1/app.js
@@ -63,7 +63,20 @@ let db;
         FOREIGN KEY (owner_id) REFERENCES Users(user_id)
     );`);
 
-    // Walker
+    // WalkRequests
+    await db.execute(`
+    CREATE TABLE WalkRequests (
+        request_id INT AUTO_INCREMENT PRIMARY KEY,
+        dog_id INT NOT NULL,
+        requested_time DATETIME NOT NULL,
+        duration_minutes INT NOT NULL,
+        location VARCHAR(255) NOT NULL,
+        status ENUM('open', 'accepted', 'completed', 'cancelled') DEFAULT 'open',Add commentMore actions
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (dog_id) REFERENCES Dogs(dog_id)
+    );`);
+
+    
 
 // Insert tables if database is empty
     // Insert Users
