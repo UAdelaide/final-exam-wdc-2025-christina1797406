f8ea1c4fde42a98b06de5582e2ef7b7982cfc312
christina1797406 <a1797406@adelaide.edu.au>
2025-06-20 06:30:31 +0000

    [automatic commit] 2025-06-20 06:30:31 UTC 203.214.152.50

--- part1/app.js
+++ part1/app.js
@@ -41,69 +41,69 @@ let db;
       database: 'DogWalkService'
     });
 
-// // Create tables if it doesn't exist
-//     // Users
-//     await db.execute(`
-//     CREATE TABLE IF NOT EXISTS Users (
-//         user_id INT AUTO_INCREMENT PRIMARY KEY,
-//         username VARCHAR(50) UNIQUE NOT NULL,
-//         email VARCHAR(100) UNIQUE NOT NULL,
-//         password_hash VARCHAR(255) NOT NULL,
-//         role ENUM('owner', 'walker') NOT NULL,
-//         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-//     );`);
+// Create tables if it doesn't exist
+    // Users
+    await db.execute(`
+    CREATE TABLE IF NOT EXISTS Users (
+        user_id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL,
+        password_hash VARCHAR(255) NOT NULL,
+        role ENUM('owner', 'walker') NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );`);
 
-//     // Dogs
-//     await db.execute(`
-//     CREATE TABLE IF NOT EXISTS Dogs (
-//         dog_id INT AUTO_INCREMENT PRIMARY KEY,
-//         owner_id INT NOT NULL,
-//         name VARCHAR(50) NOT NULL,
-//         size ENUM('small', 'medium', 'large') NOT NULL,
-//         FOREIGN KEY (owner_id) REFERENCES Users(user_id)
-//     );`);
+    // Dogs
+    await db.execute(`
+    CREATE TABLE IF NOT EXISTS Dogs (
+        dog_id INT AUTO_INCREMENT PRIMARY KEY,
+        owner_id INT NOT NULL,
+        name VARCHAR(50) NOT NULL,
+        size ENUM('small', 'medium', 'large') NOT NULL,
+        FOREIGN KEY (owner_id) REFERENCES Users(user_id)
+    );`);
 
-//     // WalkRequests
-//     await db.execute(`
-//     CREATE TABLE IF NOT EXISTS WalkRequests (
-//         request_id INT AUTO_INCREMENT PRIMARY KEY,
-//         dog_id INT NOT NULL,
-//         requested_time DATETIME NOT NULL,
-//         duration_minutes INT NOT NULL,
-//         location VARCHAR(255) NOT NULL,
-//         status ENUM('open', 'accepted', 'completed', 'cancelled') DEFAULT 'open',Add commentMore actions
-//         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-//         FOREIGN KEY (dog_id) REFERENCES Dogs(dog_id)
-//     );`);
+    // WalkRequests
+    await db.execute(`
+    CREATE TABLE IF NOT EXISTS WalkRequests (
+        request_id INT AUTO_INCREMENT PRIMARY KEY,
+        dog_id INT NOT NULL,
+        requested_time DATETIME NOT NULL,
+        duration_minutes INT NOT NULL,
+        location VARCHAR(255) NOT NULL,
+        status ENUM('open', 'accepted', 'completed', 'cancelled') DEFAULT 'open',Add commentMore actions
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (dog_id) REFERENCES Dogs(dog_id)
+    );`);
 
-//     // WalkApplication
-//     await db.execute(`
-//     CREATE TABLE IF NOT EXISTS WalkApplications
-//         application_id INT AUTO_INCREMENT PRIMARY KEY,
-//         request_id INT NOT NULL,
-//         walker_id INT NOT NULL,
-//         applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-//         status ENUM('pending', 'accepted', 'rejected') DEFAULT 'pending',
-//         FOREIGN KEY (request_id) REFERENCES WalkRequests(request_id),
-//         FOREIGN KEY (walker_id) REFERENCES Users(user_id),
-//         CONSTRAINT unique_application UNIQUE (request_id, walker_id)
-//     );`);
+    // WalkApplication
+    await db.execute(`
+    CREATE TABLE IF NOT EXISTS WalkApplications
+        application_id INT AUTO_INCREMENT PRIMARY KEY,
+        request_id INT NOT NULL,
+        walker_id INT NOT NULL,
+        applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        status ENUM('pending', 'accepted', 'rejected') DEFAULT 'pending',
+        FOREIGN KEY (request_id) REFERENCES WalkRequests(request_id),
+        FOREIGN KEY (walker_id) REFERENCES Users(user_id),
+        CONSTRAINT unique_application UNIQUE (request_id, walker_id)
+    );`);
 
-//     // WalkRatings
-//     await db.execute(`
-//     CREATE TABLE WalkRatings (
-//         rating_id INT AUTO_INCREMENT PRIMARY KEY,
-//         request_id INT NOT NULL,
-//         walker_id INT NOT NULL,Add commentMore actions
-//         owner_id INT NOT NULL,
-//         rating INT CHECK (rating BETWEEN 1 AND 5),
-//         comments TEXT,
-//         rated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-//         FOREIGN KEY (request_id) REFERENCES WalkRequests(request_id),
-//         FOREIGN KEY (walker_id) REFERENCES Users(user_id),
-//         FOREIGN KEY (owner_id) REFERENCES Users(user_id),
-//         CONSTRAINT unique_rating_per_walk UNIQUE (request_id)
-//     );`);
+    // WalkRatings
+    await db.execute(`
+    CREATE TABLE WalkRatings (
+        rating_id INT AUTO_INCREMENT PRIMARY KEY,
+        request_id INT NOT NULL,
+        walker_id INT NOT NULL,Add commentMore actions
+        owner_id INT NOT NULL,
+        rating INT CHECK (rating BETWEEN 1 AND 5),
+        comments TEXT,
+        rated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (request_id) REFERENCES WalkRequests(request_id),
+        FOREIGN KEY (walker_id) REFERENCES Users(user_id),
+        FOREIGN KEY (owner_id) REFERENCES Users(user_id),
+        CONSTRAINT unique_rating_per_walk UNIQUE (request_id)
+    );`);
 
 // Insert tables if database is empty
     // Insert Users
