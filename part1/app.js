a1c5c18a5d1b18efba340d2f041b7defd75aa9a8
christina1797406 <a1797406@adelaide.edu.au>
2025-06-20 05:33:13 +0000

    [automatic commit] 2025-06-20 05:33:13 UTC 203.214.152.50

--- part1/app.js
+++ part1/app.js
@@ -109,7 +109,7 @@ app.get('/api/walkers/summary', async (req, res) => {
 
       res.json(rows);
     } catch (err) {
-      res.status(500).json({ error: 'Failed to fetch dogs' });
+      res.status(500).json({ error: 'Failed to fetch summary of walkers' });
     }
   });
 
