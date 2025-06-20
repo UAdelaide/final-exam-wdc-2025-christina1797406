c57924b2c9c66c85139170a53c80b7e00451e5cc
christina1797406 <a1797406@adelaide.edu.au>
2025-06-20 10:19:16 +0000

    [automatic commit] 2025-06-20 10:19:16 UTC 203.214.152.50

--- part1/app.js
+++ part1/app.js
@@ -203,7 +203,7 @@ app.get('/api/walkers/summary', async (req, res) => {
       INNER JOIN Users ON Dogs.owner_id = Users.user_id )
       WHERE WalkRequests.status = 'open';
 
-      SELECT COUNT(*) AS 
+      SELECT COUNT(*) AS total_ratings FROM WalkRequests
 
 
 
