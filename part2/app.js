3a5a8107903d6a440c8d63de68bb522e6a34a40e
christina1797406 <a1797406@adelaide.edu.au>
2025-06-21 02:03:18 +0000

    [automatic commit] 2025-06-21 02:03:18 UTC 203.214.152.50

--- part2/app.js
+++ part2/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(session({
-    secret:
+    secret: 'helloworld',
     resave: false,
     saveUninitialized: false
 }));
