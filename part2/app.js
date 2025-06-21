ef2e532a8069c5de548b8228d77c76d76f957a86
christina1797406 <a1797406@adelaide.edu.au>
2025-06-21 02:02:16 +0000

    [automatic commit] 2025-06-21 02:02:16 UTC ;; communications error to 208.67.222.222#53: timed out 203.214.152.50

--- part2/app.js
+++ part2/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
+const session = require('express-session');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(session({
+    secret:
+}))
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
