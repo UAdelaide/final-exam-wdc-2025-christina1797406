b39c37b5e94e7018368b72d5a2aee2673603e2c4
christina1797406 <a1797406@adelaide.edu.au>
2025-06-21 02:02:37 +0000

    [automatic commit] 2025-06-21 02:02:37 UTC 203.214.152.50

--- part2/app.js
+++ part2/app.js
@@ -10,6 +10,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(session({
     secret:
+    resave: false,
+    saveUninitialized: false
 }))
 
 // Routes
