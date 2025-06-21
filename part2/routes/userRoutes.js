7bc5bfc3a1500c8fc9696524a9e4a893fbebe004
christina1797406 <a1797406@adelaide.edu.au>
2025-06-21 01:59:46 +0000

    [automatic commit] 2025-06-21 01:59:46 UTC ;; communications error to 208.67.222.222#53: timed out 203.214.152.50

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,7 +49,8 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    Store user 
+    // Store user login details using session
+    
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
